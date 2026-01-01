c22c6bd2bb2ee728cfd97280ee09650ddd69b274
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-01 13:43:16 +0530

    Update Products.jsx

--- Routers/Products.jsx
+++ Routers/Products.jsx
@@ -1,1 +1,15 @@
+function Products() {
+  return (
+    <div className="container">
+      <h1>Our Products</h1>
+      <ul>
+        <li>Pain Relief Medicines</li>
+        <li>Antibiotics</li>
+        <li>Healthcare Supplements</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
 
