6e088f02ac4f49484ae6bb77b11e4e6c7432ac45
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-01 13:43:43 +0530

    Add Contact component with contact information

--- Routers/Contact.jsx
+++ Routers/Contact.jsx
@@ -1,1 +1,12 @@
+function Contact() {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <p>Email: support@pharmacare.com</p>
+      <p>Phone: +91 98765 43210</p>
+    </div>
+  );
+}
+
+export default Contact;
 
