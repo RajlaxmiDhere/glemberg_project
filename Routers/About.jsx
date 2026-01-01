6124b25911f497a8bfaeb631e0035e040547f6a7
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-01 13:42:51 +0530

    Add About component with company information

--- Routers/About.jsx
+++ Routers/About.jsx
@@ -1,1 +1,14 @@
+function About() {
+  return (
+    <div className="container">
+      <h1>About Us</h1>
+      <p>
+        PharmaCare is committed to delivering safe and effective medicines to
+        improve lives.
+      </p>
+    </div>
+  );
+}
+
+export default About;
 
