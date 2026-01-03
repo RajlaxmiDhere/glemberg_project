f4151d4d43606f4f164781472c5137c523ebf193
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-03 13:09:16 +0530

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,8 +3,8 @@ import "./Products.css";
 function Products() {
   const products = [
     {
-      name: "Pain Relief",
-      info: "Fast-acting and safe pain management medicines."
+      name: "Glemvit d3",
+      info: "Bone Health: Helps the body absorb calcium and phosphorus, essential for building and maintaining strong bones and preventing bone diseases like rickets (in children) and osteomalacia (in adults).Muscle Function: Supports muscle strength and proper functioning, reducing weakness and crampsCalcium Regulation: Treats low blood calcium levels (hypocalcemia) and related conditions like hypoparathyroidism."
     },
     {
       name: "Antibiotics",
