a3f2730656e21315b690e16ec1ffec2105f10e19
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-05 11:48:36 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 function About() {
   return (
     <div className="container">
-      <h1>About Us</h1>
+      <h1>The Journey So Far</h1>
       <p>
-        PharmaCare is committed to delivering safe and effective medicines to
-        improve lives.
+        Glemberg Pharma believes that every innovation begins with a promise — a promise to improve lives and build a healthier tomorrow. Our foundation is built on strong principles of innovation, quality, safety, purity, and efficacy. Over the years, Glemberg Pharma has emerged as a trusted name in ethical pharmaceutical products across India and beyond.
+We ensure that every product meets the highest international quality standards through stringent quality control processes. Our mission is to develop and deliver innovative, safe, and high-quality medicines that address critical healthcare needs and improve patient outcomes worldwide.
+Driven by a clear and meaningful purpose, our work focuses on enriching human life through reliable and affordable healthcare solutions. We are more than just a pharmaceutical company — we are a dedicated team of scientists, researchers, and healthcare professionals working relentlessly towards a healthier future.
+Glemberg Pharma is committed to promoting good health by providing effective medicines at affordable prices. Professionally managed and quality-driven, we adhere to international standards such as WHO-GMP, offering a wide range of trusted pharmaceutical products that meet global healthcare expectations.
       </p>
     </div>
   );
