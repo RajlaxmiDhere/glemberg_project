8e5513953cfc7870887cb6196081cd20b3fdc812
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-07 11:58:16 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/about">About Us</NavLink></li>
         <li><NavLink to="/products">Products</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li><NavLink to="/contact">Contact Us</NavLink></li>
       </ul>
     </nav>
   );
