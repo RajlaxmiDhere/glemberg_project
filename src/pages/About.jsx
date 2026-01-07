8a9eccebdfc2440096c596bede870d09eb97ed29
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-07 11:49:53 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,12 +19,12 @@ function About() {
           <h2>The Journey So Far</h2>
           <p>
            <strong>Glemberg Pharma</strong> believes that every innovation begins with a promise a promise to improve lives and build a healthier tomorrow. Our foundation is built on strong principles of innovation, quality, safety, purity, and efficacy. Over the years, Glemberg Pharma has emerged as a trusted name in ethical pharmaceutical products across India and beyond.
-
+<br></br>
 We ensure that every product meets the highest international quality standards through stringent quality control processes. Our mission is to develop and deliver innovative, safe, and high-quality medicines that address critical healthcare needs and improve patient outcomes worldwide.
           </p>
           <p>
           Driven by a clear and meaningful purpose, our work focuses on enriching human life through reliable and affordable healthcare solutions. We are more than just a pharmaceutical company — we are a dedicated team of scientists, researchers, and healthcare professionals working relentlessly towards a healthier future.
-
+<br></br>
 Glemberg Pharma is committed to promoting good health by providing effective medicines at affordable prices. Professionally managed and quality-driven, we adhere to international standards such as <b>WHO-GMP</b>, offering a wide range of trusted pharmaceutical products that meet global healthcare expectations.
           </p>
         </div>
