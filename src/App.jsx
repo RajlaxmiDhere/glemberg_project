9abeb46b191ff1b3526848ab96ee0021b66a4fd3
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-08 13:11:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -6,60 +5,32 @@ import About from "./pages/About";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
 import "./App.css";
+<header>
+  {/* The Top Line with Logos */}
+  <div className="top-social-bar">
+    <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" className="social-logo" alt="LinkedIn" />
+    </a>
+    <a href="https://instagram.com" target="_blank" rel="noreferrer">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" className="social-logo" alt="Instagram" />
+    </a>
+    <a href="https://facebook.com" target="_blank" rel="noreferrer">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg" className="social-logo" alt="Facebook" />
+    </a>
+  </div>
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="app-main-layout">
-        
-        {/* 1. TOP SOCIAL BAR - Full Width with Large Icons */}
-        <div className="top-social-bar">
-          <div className="social-icons-container">
-            {/* LinkedIn */}
-            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-              <img 
-                src="https://cdn-icons-png.flaticon.com/512/145/145807.png" 
-                className="social-logo" 
-                alt="LinkedIn" 
-              />
-            </a>
-
-            {/* Instagram */}
-            <a href="https://instagram.com" target="_blank" rel="noreferrer">
-              <img 
-                src="https://cdn-icons-png.flaticon.com/512/3955/3955024.png" 
-                className="social-logo" 
-                alt="Instagram" 
-              />
-            </a>
-
-            {/* Facebook */}
-            <a href="https://facebook.com" target="_blank" rel="noreferrer">
-              <img 
-                src="https://cdn-icons-png.flaticon.com/512/145/145802.png" 
-                className="social-logo" 
-                alt="Facebook" 
-              />
-            </a>
-          </div>
-        </div>
-
-        {/* 2. NAVIGATION BAR */}
-        <Navbar />
-
-        {/* 3. PAGE CONTENT ROUTES */}
-        <div className="content-area">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-
-      </div>
-    </BrowserRouter>
-  );
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App;
+export default App;these is app,jsx
