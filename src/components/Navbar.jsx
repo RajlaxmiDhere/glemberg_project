f24fc363ca0b814706a40b02a3e075511e36b8b2
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-08 22:07:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,29 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/combined.jpg";
 import "./navbar.css";
 
 function Navbar() {
-  const location = useLocation();
-  const isHome = location.pathname === "/";
-
   return (
     <>
-      {/* Top Social Bar – only on Home */}
-      {isHome && (
-        <div className="top-social-bar">
-          <div className="social-bar-inner">
-            <a href="#" className="social-icon">
-              <i className="bi bi-instagram"></i>
-            </a>
-            <a href="#" className="social-icon">
-              <i className="bi bi-whatsapp"></i>
-            </a>
-            <a href="#" className="social-icon">
-              <i className="bi bi-linkedin"></i>
-            </a>
-          </div>
+      {/* TOP SOCIAL BAR */}
+      <div className="top-social-bar">
+        <div className="social-bar-inner">
+          <a href="https://instagram.com" target="_blank" rel="noreferrer">
+            <i className="bi bi-instagram"></i>
+          </a>
+          <a href="https://wa.me/918010393478" target="_blank" rel="noreferrer">
+            <i className="bi bi-whatsapp"></i>
+          </a>
+          <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+            <i className="bi bi-linkedin"></i>
+          </a>
         </div>
-      )}
+      </div>
 
-      {/* Navbar */}
+      {/* NAVBAR */}
       <nav className="navbar">
         <div className="navbar-inner">
-          <img src={logo} alt="Company Logo" className="navbar-logo" />
+          <img src={logo} alt="Glemberg Pharma" className="navbar-logo" />
 
           <ul className="nav-links">
             <li><NavLink to="/">Home</NavLink></li>
