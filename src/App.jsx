312ef13eba2401cc1f0ace17790cfb18042ad903
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-08 12:50:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,29 @@ import About from "./pages/About";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
 import "./App.css";
-<header>
-  {/* The Top Line with Logos */}
-  <div className="top-social-bar">
-    <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" className="social-logo" alt="LinkedIn" />
-    </a>
-    <a href="https://instagram.com" target="_blank" rel="noreferrer">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" className="social-logo" alt="Instagram" />
-    </a>
-    <a href="https://facebook.com" target="_blank" rel="noreferrer">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg" className="social-logo" alt="Facebook" />
-    </a>
-  </div>
 
 function App() {
   return (
     <BrowserRouter>
+      {/* 1. The Top Line with Logos - Now inside the return and wrapped */}
+      <div className="top-social-bar">
+        <div className="social-icons-container">
+          <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" className="social-logo" alt="LinkedIn" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" className="social-logo" alt="Instagram" />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg" className="social-logo" alt="Facebook" />
+          </a>
+        </div>
+      </div>
+
+      {/* 2. Navigation Menu */}
       <Navbar />
+
+      {/* 3. Page Content */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
