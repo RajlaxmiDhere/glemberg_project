2b5866454dbcaf3912dac5eaa1445d33796e148e
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-08 22:23:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,38 +4,20 @@ import "./navbar.css";
 
 function Navbar() {
   return (
-    <>
-      {/* TOP SOCIAL BAR */}
-      <div className="top-social-bar">
-        <div className="top-social-inner">
-          <a href="https://instagram.com" target="_blank" rel="noreferrer">
-            <i className="bi bi-instagram"></i>
-          </a>
-          <a href="https://wa.me/918010393478" target="_blank" rel="noreferrer">
-            <i className="bi bi-whatsapp"></i>
-          </a>
-          <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-            <i className="bi bi-linkedin"></i>
-          </a>
-        </div>
+    <nav className="navbar">
+      <div className="navbar-inner">
+        {/* LEFT: Company Logo */}
+        <img src={logo} alt="Glemberg Pharma" className="navbar-logo" />
+
+        {/* RIGHT: Menu */}
+        <ul className="nav-links">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About Us</NavLink></li>
+          <li><NavLink to="/products">Products</NavLink></li>
+          <li><NavLink to="/contact">Contact Us</NavLink></li>
+        </ul>
       </div>
-
-      {/* NAVBAR */}
-      <nav className="navbar">
-        <div className="navbar-inner">
-          {/* LEFT: Company Logo */}
-          <img src={logo} alt="Glemberg Pharma" className="navbar-logo" />
-
-          {/* RIGHT: Menu */}
-          <ul className="nav-links">
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/about">About Us</NavLink></li>
-            <li><NavLink to="/products">Products</NavLink></li>
-            <li><NavLink to="/contact">Contact Us</NavLink></li>
-          </ul>
-        </div>
-      </nav>
-    </>
+    </nav>
   );
 }
 
