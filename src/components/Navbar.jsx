510f8c4cc6c99ff41d1bfd70e455f1481056fdba
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-08 21:37:06 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,63 @@
-import { NavLink } from "react-router-dom";
-import logo from "../assets/combined.jpg";   
+import { NavLink, useLocation } from "react-router-dom";
+import logo from "../assets/combined.jpg";
 import "./navbar.css";
 
 function Navbar() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
-    <nav className="navbar">
-      {/* Top Row */}
-      <div className="navbar-top">
-        {/* Left: Logo */}
-        <div className="logo-container">
-          <img src={logo} alt="Company Logo" className="logo-img" />
-          
+    <>
+      {/* Top Social Bar – ONLY on Home */}
+      {isHome && (
+        <div className="top-social-bar">
+          <div className="container d-flex justify-content-end gap-4">
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noreferrer"
+              className="text-white fs-5"
+            >
+              <i className="bi bi-instagram"></i>
+            </a>
+
+            <a
+              href="https://wa.me/918010393478"
+              target="_blank"
+              rel="noreferrer"
+              className="text-white fs-5"
+            >
+              <i className="bi bi-whatsapp"></i>
+            </a>
+
+            <a
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noreferrer"
+              className="text-white fs-5"
+            >
+              <i className="bi bi-linkedin"></i>
+            </a>
+          </div>
         </div>
+      )}
+
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="navbar-top">
+          <div className="logo-container">
+            <img src={logo} alt="Company Logo" className="logo-img" />
+          </div>
         </div>
-    
-      {/* Bottom Row */}
-      <ul className="nav-links">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About Us</NavLink></li>
-        <li><NavLink to="/products">Products</NavLink></li>
-        <li><NavLink to="/contact">Contact Us</NavLink></li>
-      </ul>
-    </nav>
+
+        <ul className="nav-links">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About Us</NavLink></li>
+          <li><NavLink to="/products">Products</NavLink></li>
+          <li><NavLink to="/contact">Contact Us</NavLink></li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
