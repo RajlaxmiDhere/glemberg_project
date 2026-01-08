b9b2f35d7f7f8efc1037bf0145b436e43936683d
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-08 13:12:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar"; // Adjust path if needed
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Products from "./pages/Products";
 import Contact from "./pages/Contact";
 import "./App.css";
-<header>
-  {/* The Top Line with Logos */}
-  <div className="top-social-bar">
-    <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" className="social-logo" alt="LinkedIn" />
-    </a>
-    <a href="https://instagram.com" target="_blank" rel="noreferrer">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" className="social-logo" alt="Instagram" />
-    </a>
-    <a href="https://facebook.com" target="_blank" rel="noreferrer">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg" className="social-logo" alt="Facebook" />
-    </a>
-  </div>
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        {/* 1. TOP SOCIAL BAR (This creates the top line) */}
+        <div className="top-social-bar">
+          <div className="social-icons-wrapper">
+            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+              <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+              <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" />
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
+              <img src="https://cdn-icons-png.flaticon.com/512/174/174848.png" alt="Facebook" />
+            </a>
+          </div>
+        </div>
+
+        {/* 2. NAVBAR */}
+        <Navbar />
+
+        {/* 3. PAGE ROUTES */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App;these is app,jsx
+export default App;
