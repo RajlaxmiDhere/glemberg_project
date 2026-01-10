a8b632e6771be4dfdf2987675abc87eb38facf39
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-10 11:49:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
