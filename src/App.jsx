c81625f9466874fa0c276b8e49f668326c476305
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-10 11:52:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+
 import "./App.css";
 function App() {
   return (
