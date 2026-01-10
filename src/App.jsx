b0ec82c53b93e7fc3e22e5acd28916394a07650f
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-10 13:30:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"
       alt="Instagram"
-      style={{ width: "50px", height: "50px", display: "block", objectFit: "contain" }}
+      style={{ width: "26px", height: "26px", display: "block", objectFit: "contain" }}
     />
   </a>
 
