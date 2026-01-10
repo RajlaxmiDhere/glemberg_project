773fe6621370c99300349e86560b9f060ff088a2
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-10 13:34:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
       alt="Whatsapp"
-      style={{ width: "26px", height: "26px", display: "block", objectFit: "contain" }}
+      style={{ width: "28px", height: "28px", display: "block", objectFit: "contain" }}
     />
   </a>
 
