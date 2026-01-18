67c2f385f00f1a4b4724025287b1b7effe717e77
Shruti Gavali <shrutigavali03@gmail.com>
2026-01-18 12:15:20 +0530

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -52,7 +52,7 @@ const Product = () => {
       division: "GENERAL RANGE", 
       image:AceglemMR
     },
-    { id: 6, name: "AceglemSP", desc: "", category: "general", division: "GENERAL RANGE", image: "Aceglem_SP_tab.png" },
+    { id: 6, name: "Aceglem_SP_tab", desc: "", category: "general", division: "GENERAL RANGE", image: "Aceglem_SP" },
   ];
 
   const filteredProducts = filter === 'all' 
