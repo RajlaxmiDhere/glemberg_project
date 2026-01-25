75f9c15a4b872dbc839caf3463b3a57deff4d4d4
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-25 12:06:18 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,21 @@
+const mysql = require("mysql2");
+
+// Create MySQL connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "YOUR_MYSQL_PASSWORD",
+  database: "glemberg_db"
+});
+
+// Connect
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL Connection Failed:", err);
+  } else {
+    console.log("MySQL Connected");
+  }
+});
+
+module.exports = db;
 
