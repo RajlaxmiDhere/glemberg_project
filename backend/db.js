2656dca42ca6050f1375ecc89d29af43a4b1078b
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-01-25 12:07:47 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "YOUR_MYSQL_PASSWORD",
+  password: "rajlaxmi",
   database: "glemberg_db"
 });
 
