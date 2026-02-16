a0e70097275555802c2e5e24a71c3840447692d4
Shruti Gavali <shrutigavali03@gmail.com>
2026-02-16 22:24:04 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import { NavLink } from "react-router-dom";
 // Assets
 import featureImg from "../assets/feature-medical.jpg";
 import dermaImg from "../assets/derma_division.jpg";
-import generalImg from "../assets/general_range.jpg";
+import generalImg from "../assets/general_range1.jpg";
 import orthoImg from "../assets/ortho_division.jpg";
 import certificate from "../assets/Certificate of Incorporation Glemberg.pdf";
 
