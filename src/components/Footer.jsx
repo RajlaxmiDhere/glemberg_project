604ea5b89f527e64dbbe0beae3dc7fe5206ee162
Shruti Gavali <shrutigavali03@gmail.com>
2026-02-17 14:48:05 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,8 +34,6 @@ function Footer() {
           <p className="footer-contact">
             <FaPhoneAlt />{" "}
             <a href="tel:+917559189020">+91 7559189020</a><br />
-            <FaPhoneAlt />{" "}
-            <a href="tel:+918975173157">+91 8975173157</a>
           </p>
 
           <h4>Email</h4>
