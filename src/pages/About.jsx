b0ba163f1dcb155d380092a9abd50ade0f7e8d79
Shruti Gavali <shrutigavali03@gmail.com>
2026-02-17 14:04:01 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -56,8 +56,8 @@ function About() {
           <h3>Making a Positive Contribution to Society</h3>
           <p>
             Our goal is to make a positive contribution to the society by providing every customer with product and service representing the best combination of performance and value.
-
-Our goal is to contribute positively to society by delivering products and services that embody the perfect balance of performance and value.
+</p>
+<p>Our goal is to contribute positively to society by delivering products and services that embody the perfect balance of performance and value.
           </p>
         </div>
       </section>
@@ -71,8 +71,8 @@ Our goal is to contribute positively to society by delivering products and servi
         <h1>Our Core Values</h1>
           <h3>Incorporating Core Values into Business Practices</h3>
           <p>Honesty, humility, integrity, quality, collaboration, timely service and customer satisfaction. We believe that these values are the foundation of a successful business.
-
-We believe that honesty, humility, integrity, quality, collaboration, timely service, and customer satisfaction are the pillars that drive a truly successful business.</p>
+</p>
+<p>We believe that honesty, humility, integrity, quality, collaboration, timely service, and customer satisfaction are the pillars that drive a truly successful business.</p>
         </div>
       </section>
 
