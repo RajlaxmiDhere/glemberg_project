532964a837ba392568ee9050637b5a8fc73b17b4
RajlaxmiDhere <157942827+RajlaxmiDhere@users.noreply.github.com>
2026-03-15 13:21:20 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,23 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo (2).jpg";
 import "./navbar.css";
 
 function Navbar() {
-
-const [menuOpen, setMenuOpen] = useState(false);
-
 return ( <nav className="navbar"> <div className="navbar-inner"> <div className="logo-group"> <img src={logo} alt="Glemberg Pharma" className="navbar-logo" /> <span className="navbar-tagline">Science with a Human Touch</span> </div>
 
-    {/* HAMBURGER BUTTON */}
-    <div
-      className="hamburger"
-      onClick={() => setMenuOpen(!menuOpen)}
-    >
-      ☰
-    </div>
-
+```
     {/* RIGHT MENU */}
-    <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+    <ul className="nav-links">
       <li><NavLink to="/">Home</NavLink></li>
       <li><NavLink to="/about">About Us</NavLink></li>
       <li><NavLink to="/products">Products</NavLink></li>
       <li><NavLink to="/contact">Contact Us</NavLink></li>
     </ul>
-
   </div>
 </nav>
+```
+
 );
 }
+
 export default Navbar;
